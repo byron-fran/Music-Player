0b2cc48f4012ca702ac6cfb2567905c69eacecf0
byron <1byron1999@gmail.com>
2025-01-05 13:53:20 -0800

    Add Song List Component

---
+++ app/src/main/java/com/byrondev/musicplayer/components/songs/SongList.kt
@@ -0,0 +1,54 @@
+package com.byrondev.musicplayer.components.songs
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.byrondev.musicplayer.components.images.CoverImage
+import com.byrondev.musicplayer.data.models.Song
+import com.byrondev.musicplayer.viewModels.PlayerViewModels
+
+
+@RequiresApi(Build.VERSION_CODES.S)
+@Composable
+fun SongList(
+    songs : List<Song>,
+    showTrackNumber : Boolean = true,
+    paddingValues: PaddingValues,
+    playerViewModels : PlayerViewModels
+    ){
+
+    Column (modifier = Modifier.background(color = Color.Black).fillMaxSize().padding(paddingValues)){
+        LazyColumn (
+            modifier = Modifier.padding(top = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+
+            ) {
+
+            itemsIndexed(songs) {index, song ->
+                Row (verticalAlignment = Alignment.CenterVertically) {
+                    CoverImage(song.cover, Modifier.height(55.dp).width(55.dp).align(Alignment.CenterVertically).padding(vertical = 2.dp), contentScale = ContentScale.Fit)
+                    SongCard (song,showTrackNumber,) {
+
+                    }
+                }
+            }
+        }
+    }
+
+}
