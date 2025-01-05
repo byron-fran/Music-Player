70a45cc000604d1533e71ac5ef84d71dace2369a
byron <1byron1999@gmail.com>
2025-01-05 13:56:06 -0800

    Add Song Dao

---
+++ app/src/main/java/com/byrondev/musicplayer/data/dao/SongDao.kt
@@ -0,0 +1,26 @@
+package com.byrondev.musicplayer.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.byrondev.musicplayer.data.models.Song
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface SongDao {
+    @Query("SELECT * FROM songs ORDER by title ASC")
+    fun getAllSongs() : Flow<List<Song>>
+
+    @Query("SELECT * FROM songs WHERE album_id=:id")
+    fun  getAlbumWithSong(id : Int) : Flow<List<Song>>
+
+    @Query("SELECT * FROM songs WHERE title=:title AND album=:album")
+    suspend fun getSongByTitle(title : String, album : String) : Song?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSong(song: Song) : Long
+
+
+}
