03ce723c6474136c09355e5f47cd7acb801a23a2
byron <1byron1999@gmail.com>
2025-01-05 09:22:38 -0800

    Add Album List component

---
+++ app/src/main/java/com/byrondev/musicplayer/components/albums/AlbumsList.kt
@@ -0,0 +1,40 @@
+package com.byrondev.musicplayer.components.albums
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.byrondev.musicplayer.data.models.Album
+
+
+@Composable
+fun AlbumsList(albums: List<Album>, navController: NavController, paddingValues : PaddingValues) {
+
+    Column (modifier = Modifier.background(color = Color.Black).padding(paddingValues).fillMaxSize()){
+
+        LazyVerticalGrid  (
+            columns = GridCells.Adaptive(170.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp),
+
+            ) {
+
+            items(albums){album ->
+                AlbumCard(album, navController)
+            }
+        }
+    }
+
+
+}
