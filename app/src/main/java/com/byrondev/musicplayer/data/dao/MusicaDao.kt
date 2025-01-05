044e1a730dfa5340ded9b08cac7bcf7638c9b933
byron <1byron1999@gmail.com>
2025-01-05 15:45:46 -0800

    Add Music Dao

---
+++ app/src/main/java/com/byrondev/musicplayer/data/dao/MusicaDao.kt
@@ -0,0 +1,55 @@
+package com.byrondev.musicplayer.data.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+data class SearchResult(
+    val name: String,
+    val artist : String?,
+    val album : String?,
+    val uri : String?,
+    val id: Int,
+    val cover: ByteArray?,
+    val type: String,
+    val audioBitDepth : Int?
+
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SearchResult
+
+        if (cover != null) {
+            if (other.cover == null) return false
+            if (!cover.contentEquals(other.cover)) return false
+        } else if (other.cover != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return cover?.contentHashCode() ?: 0
+    }
+}
+
+@Dao
+interface MusicDao {
+
+    @Query("""
+            SELECT title AS name, artist,cover, album, uri, audio_bit_depth as audioBitDepth,  id, 'Song' AS type
+            FROM songs
+            WHERE LOWER(title) LIKE LOWER('%' || :query || '%')
+            UNION
+            SELECT albums.title AS name, albums.album_artist as artist, cover, "" as album, "" as uri, "" as audio_bit_depth, id,  'Album' AS type
+            FROM albums
+            WHERE LOWER(albums.title) LIKE  LOWER('%' || :query || '%') 
+            UNION
+            SELECT artists.name AS name, "" as artist, cover, "" as album, "" as uri, "" as audio_bit_depth, artists.id AS id, 'Artist' AS type
+            FROM artists
+            WHERE  LOWER(artists.name) LIKE  LOWER('%' || :query || '%'); 
+            
+            """)
+    fun searchSong(query : String) : Flow<List<SearchResult>>
+}
