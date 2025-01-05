82e9f5662938de95e77427f995d19d8656ff2b05
byron <1byron1999@gmail.com>
2025-01-05 07:52:23 -0800

    Add Main activity

---
+++ app/src/main/java/com/byrondev/musicplayer/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.byrondev.musicplayer
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
+import androidx.media3.exoplayer.ExoPlayer
+import com.byrondev.musicplayer.navigation.NavManager
+import com.byrondev.musicplayer.ui.theme.MusicPlayerTheme
+import com.byrondev.musicplayer.viewModels.MusicViewModels
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    private lateinit var player: ExoPlayer
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val viewModels: MusicViewModels by viewModels()
+
+        // Create the ExoPlayer instance
+        player = ExoPlayer.Builder(this)
+            .setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setUsage(C.USAGE_MEDIA)
+                    .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+                    .build(),
+                true
+            )
+            .setHandleAudioBecomingNoisy(true)
+            .build()
+
+        enableEdgeToEdge()
+
+        setContent {
+            MusicPlayerTheme {
+                NavManager(viewModels, player)
+            }
+        }
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        player.release()
+    }
+}
+
+
+
+
+
