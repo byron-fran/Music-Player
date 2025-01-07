91109db3acb83d36e930b2a9891fe14af3b11308
byron <1byron1999@gmail.com>
2025-01-06 21:29:53 -0800

    Call getAllSongs in launched effect

--- app/src/main/java/com/byrondev/musicplayer/views/songs/SongsScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/songs/SongsScreen.kt
@@ -4,6 +4,8 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.byrondev.musicplayer.components.songs.SongList
@@ -14,7 +16,11 @@ import com.byrondev.musicplayer.viewModels.PlayerViewModels
 @Composable
 fun SongsScreen (musicViewModels: MusicViewModels, navController: NavController,playerViewModels : PlayerViewModels ) {
 
-//    val listSong = musicViewModels.songs.collectAsState().value // Todo update this
-    SongList(emptyList(), /* Todo add List song real */false, PaddingValues(0.dp),playerViewModels)
+    LaunchedEffect(Unit) {
+        musicViewModels.getAllSongs()
+    }
+
+    val listSong = musicViewModels.songs.collectAsState() // Todo update this
+    SongList(listSong.value, showTrackNumber = false, PaddingValues(0.dp), playerViewModels)
 
 }
