a88b7ad58c96f4a4c3fef0fb4feaf1ee26110581
byron <1byron1999@gmail.com>
2025-01-08 14:56:14 -0800

    Add playback_queue in MusicDB

--- app/src/main/java/com/byrondev/musicplayer/data/database/MusicDB.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/database/MusicDB.kt
@@ -4,15 +4,18 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.byrondev.musicplayer.data.dao.AlbumsDao
 import com.byrondev.musicplayer.data.dao.ArtistsDao
+import com.byrondev.musicplayer.data.dao.PlaybackQueueDao
 import com.byrondev.musicplayer.data.dao.SongDao
 import com.byrondev.musicplayer.data.models.Album
 import com.byrondev.musicplayer.data.models.Artist
+import com.byrondev.musicplayer.data.models.PlaybackQueue
 import com.byrondev.musicplayer.data.models.Song
 
-@Database(entities = [Album::class, Artist::class, Song::class], version = 35)
+@Database(entities = [Album::class, Artist::class, Song::class, PlaybackQueue::class], version = 47)
 abstract class MusicDB : RoomDatabase() {
     abstract fun albumDao () : AlbumsDao
     abstract fun songDao () : SongDao
     abstract fun artistDao () : ArtistsDao
+    abstract fun playbackQueueDao () : PlaybackQueueDao
 
 }
