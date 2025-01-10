4497ded8cb4ad36b6f00ecd57a10630cb2dce63d
byron <1byron1999@gmail.com>
2025-01-09 21:36:14 -0800

    Update font style and cover image of album card

--- app/src/main/java/com/byrondev/musicplayer/components/albums/AlbumCard.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/albums/AlbumCard.kt
@@ -1,7 +1,5 @@
 package com.byrondev.musicplayer.components.albums
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -10,18 +8,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -30,19 +24,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.byrondev.musicplayer.R
+import com.byrondev.musicplayer.components.globals.CircleSeparation
+import com.byrondev.musicplayer.components.globals.TextExtraSmall
+import com.byrondev.musicplayer.components.images.CoverImage
 import com.byrondev.musicplayer.data.models.Album
-import com.byrondev.musicplayer.ui.theme.Gray20
 
-
-fun byteArrayToBitmap(byteArray: ByteArray): Bitmap {
-    return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-}
 
 @Composable
 fun AlbumCard(album: Album, navController: NavController) {
 
-    val albumArtBitmap = album.cover?.let { byteArrayToBitmap(it) }
-    // Todo update how get album Cover
     Card(
         modifier = Modifier
             .clickable { navController.navigate("AlbumDetail/${album.id}") }
@@ -51,50 +41,31 @@ fun AlbumCard(album: Album, navController: NavController) {
         shape = RoundedCornerShape(5.dp)
     ) {
 
-        if (albumArtBitmap != null) {
-            Image(
-                bitmap = albumArtBitmap.asImageBitmap(), contentDescription = "",
-                modifier = Modifier
-                    .clip(RoundedCornerShape(5.dp))
-                    .fillMaxWidth(),
-                contentScale = ContentScale.Crop
-            )
-        } else {
-            Image(
-                imageVector = Icons.Default.Email, contentDescription = "Album default image",
-                modifier = Modifier
-                    .clip(RoundedCornerShape(5.dp))
-                    .fillMaxWidth(),
-                contentScale = ContentScale.Crop
-            )
-
-        }
+        CoverImage(album.cover, contentScale = ContentScale.Crop)
         Row(
-            modifier = Modifier
-                .padding(top = 5.dp)
-                .fillMaxWidth(),
+            modifier = Modifier.padding(top = 5.dp).fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Column(modifier = Modifier) {
+            Column(modifier = Modifier.weight(1f)) {
 
-                Text(
-                    album.title ?: "Unknown Album",
+                Text(album.title ?: "Unknown Album",
                     color = Color.White,
-                    modifier = Modifier.width(140.dp),
                     maxLines = 1,
                     minLines = 1,
                     overflow = TextOverflow.Ellipsis,
                     fontSize = 17.sp,
                     fontWeight = FontWeight.Bold
                 )
-                Text(
-                    album.artist ?: "",
-                    color = Gray20,
-                    maxLines = 1,
-                    minLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    fontSize = 17.sp
-                )
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    TextExtraSmall(text=album.artist ?: "")
+                    if(album.year != null) {
+                        CircleSeparation()
+                        TextExtraSmall(text=album.year.substringBefore("-"))
+                    }
+                }
             }
 
             if(album.quality == 3) {
@@ -104,6 +75,7 @@ fun AlbumCard(album: Album, navController: NavController) {
                     modifier = Modifier.size(35.dp)
                 )
             }
+
         }
 
     }
