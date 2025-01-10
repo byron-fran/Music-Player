c857f61a486edf5e6c5d6e4f4fca8385f47939e1
byron <1byron1999@gmail.com>
2025-01-09 18:02:43 -0800

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.android.build.api.dsl.Packaging
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -71,6 +69,10 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.animation.core.android)
+    implementation(libs.androidx.foundation.layout.android)
+    implementation(libs.androidx.foundation.layout.android)
+    implementation(libs.androidx.foundation.layout.android)
+    implementation(libs.androidx.foundation.layout.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -128,5 +130,8 @@ dependencies {
 
     // Preferences data store
     implementation(libs.androidx.datastore.preferences)
+
+//    ToolbarCol
+    implementation(libs.collapsingtopbarcompose)
 }
 
