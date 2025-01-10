93f4d106bb240232d7aa328d0eea47f7fb55706c
byron <1byron1999@gmail.com>
2025-01-10 13:40:33 -0800

    Add PlayList dao and PlaylistSongCrossRef in musicDB

--- app/src/main/java/com/byrondev/musicplayer/data/database/MusicDB.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/database/MusicDB.kt
@@ -5,17 +5,29 @@ import androidx.room.RoomDatabase
 import com.byrondev.musicplayer.data.dao.AlbumsDao
 import com.byrondev.musicplayer.data.dao.ArtistsDao
 import com.byrondev.musicplayer.data.dao.PlaybackQueueDao
+import com.byrondev.musicplayer.data.dao.PlaylistDao
 import com.byrondev.musicplayer.data.dao.SongDao
 import com.byrondev.musicplayer.data.models.Album
 import com.byrondev.musicplayer.data.models.Artist
 import com.byrondev.musicplayer.data.models.PlaybackQueue
+import com.byrondev.musicplayer.data.models.Playlist
+import com.byrondev.musicplayer.data.models.PlaylistSongCrossRef
 import com.byrondev.musicplayer.data.models.Song
 
-@Database(entities = [Album::class, Artist::class, Song::class, PlaybackQueue::class], version = 47)
+@Database(entities =
+[
+    Album::class,
+    Artist::class,
+    Song::class,
+    PlaybackQueue::class,
+    Playlist::class,
+    PlaylistSongCrossRef::class
+], version = 48)
 abstract class MusicDB : RoomDatabase() {
     abstract fun albumDao () : AlbumsDao
     abstract fun songDao () : SongDao
     abstract fun artistDao () : ArtistsDao
     abstract fun playbackQueueDao () : PlaybackQueueDao
+    abstract fun playlistDao(): PlaylistDao
 
 }
