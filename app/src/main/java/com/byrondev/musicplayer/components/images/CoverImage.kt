cf483f83dd614d8ce377800f7404f8eaaca877b0
byron <1byron1999@gmail.com>
2025-01-11 11:23:24 -0800

    Update cover image

--- app/src/main/java/com/byrondev/musicplayer/components/images/CoverImage.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/images/CoverImage.kt
@@ -2,17 +2,12 @@ package com.byrondev.musicplayer.components.images
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -39,20 +34,12 @@ fun CoverImage(byteArray: ByteArray?, modifier: Modifier = Modifier, contentScal
     else {
         Box(
             modifier= modifier,
-            contentAlignment = Alignment.Center,
+            contentAlignment = Alignment.TopStart,
 
         ) {
-            Card (
-                modifier = Modifier.fillMaxSize(),
-                shape = RoundedCornerShape(5.dp),
-                colors = CardDefaults.cardColors( /* Todo add color container */ contentColor =  Color.White)
-                ) {}
             Image(
                 painter = painterResource(id= R.drawable.baseline_music_note_600),
-                modifier = Modifier.size(200.dp),
-                contentDescription = "Cover Image default",
-
-                )
+                contentDescription = "Cover Image default",)
         }
     }
 }
