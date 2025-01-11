2950cc2786f40227499ee62ed504eb02afd68444
byron <1byron1999@gmail.com>
2025-01-11 11:14:19 -0800

    Update changes in SongsByArtistScreen

--- app/src/main/java/com/byrondev/musicplayer/views/songs/SongsByArtistScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/songs/SongsByArtistScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
 import com.byrondev.musicplayer.components.BottomBar
 import com.byrondev.musicplayer.components.songs.SongList
@@ -14,25 +16,22 @@ import com.byrondev.musicplayer.components.topbar.CenterTopAppBar
 import com.byrondev.musicplayer.viewModels.MusicViewModels
 import com.byrondev.musicplayer.viewModels.PlayerViewModels
 
-
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
 fun SongsByArtist(navController: NavController, musicViewModels: MusicViewModels, playerViewModels : PlayerViewModels, id : Int, ) {
 
-    // Todo Update This
-//     val artistWithSongs  by musicViewModels.artistWithSongs.collectAsState()
-
-//    val songs = artistWithSongs!!.songs.sortedByDescending {  it.year?.substringBefore("-")?.toInt()  }
+    val artistWithSongs  by musicViewModels.artistWithSongs.collectAsState()
+    val songs = artistWithSongs?.songs?.sortedByDescending {  it.year?.substringBefore("-")?.toInt()  } ?: emptyList()
 
     LaunchedEffect(id) {
 
-        musicViewModels.getArtistByIdWithSong(id)
+        musicViewModels.getArtistWithSongs(id)
     }
 
     Scaffold(
         topBar = { CenterTopAppBar("Songs", Icons.AutoMirrored.Default.ArrowBack, onNavigate = { navController.popBackStack()}) },
         content = {paddingValues ->
-                SongList(emptyList(), /* Todo add list Song*/false, paddingValues, playerViewModels)
+                SongList(songs, showTrackNumber = false, paddingValues, playerViewModels)
         },
         bottomBar = { BottomBar(navController) }
     )
