6a484cb5d9383af5dd7454b452ddefa9c0d185e0
byron <1byron1999@gmail.com>
2025-01-10 16:27:42 -0800

    Update text styles component

--- app/src/main/java/com/byrondev/musicplayer/components/globals/TextStyles.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/globals/TextStyles.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import com.byrondev.musicplayer.ui.theme.Zinc40
 import com.byrondev.musicplayer.ui.theme.textExtraSmall
+import com.byrondev.musicplayer.ui.theme.textMedium
 
 @Composable
 fun TextExtraSmall(modifier: Modifier = Modifier,text : String, color: Color = Zinc40,  ) {
@@ -18,6 +19,18 @@ fun TextExtraSmall(modifier: Modifier = Modifier,text : String, color: Color = Z
         minLines = 1,
         overflow = TextOverflow.Ellipsis,
         style = textExtraSmall,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun TextMedium(text : String ,modifier: Modifier = Modifier, color: Color = Color.White) {
+    Text(
+        text,
+        color = color,
+        style = textMedium,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
         modifier = modifier
     )
 }
