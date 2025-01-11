432d3afc2553f3d9d5ce0ecba9ce810f08c8e372
byron <1byron1999@gmail.com>
2025-01-11 11:21:37 -0800

    update Foreignkeys relation in ArtistWithSongs

--- app/src/main/java/com/byrondev/musicplayer/data/relations/ArtistWithSongs.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/relations/ArtistWithSongs.kt
@@ -2,15 +2,14 @@ package com.byrondev.musicplayer.data.relations
 
 import androidx.room.Embedded
 import androidx.room.Relation
-import com.byrondev.musicplayer.data.models.Album
 import com.byrondev.musicplayer.data.models.Artist
 import com.byrondev.musicplayer.data.models.Song
 
 data class ArtistWithSongs(
-    @Embedded val song: Song,
+    @Embedded val arist: Artist,
     @Relation(
         parentColumn = "id",
-        entityColumn = "song_id"
+        entityColumn = "artist_id"
     )
     val songs: List<Song>
 )
