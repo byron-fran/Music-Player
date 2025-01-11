fecbf3a6ac8defcf0666ae95364faa0a6f705fa9
byron <1byron1999@gmail.com>
2025-01-11 11:17:31 -0800

    Add relation model/table PlaylistWithSongs

---
+++ app/src/main/java/com/byrondev/musicplayer/data/relations/PlaylistWithSongs.kt
@@ -0,0 +1,23 @@
+package com.byrondev.musicplayer.data.relations
+
+import androidx.room.Embedded
+import androidx.room.Junction
+import androidx.room.Relation
+import com.byrondev.musicplayer.data.models.Playlist
+import com.byrondev.musicplayer.data.models.PlaylistSongCrossRef
+import com.byrondev.musicplayer.data.models.Song
+
+data class PlaylistWithSongs(
+    @Embedded val playlist: Playlist,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "id",
+        associateBy = Junction(
+            PlaylistSongCrossRef::class,
+            parentColumn = "playlistId",
+            entityColumn = "songId"
+            )
+    )
+    val songs: List<Song>
+)
+
