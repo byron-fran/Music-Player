03d95174121bcf5aaa00b632b0773f33ffa4c83a
byron <1byron1999@gmail.com>
2025-01-11 11:28:15 -0800

    Update layout artist screen

--- app/src/main/java/com/byrondev/musicplayer/views/artists/ArtistScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/artists/ArtistScreen.kt
@@ -9,32 +9,42 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.byrondev.musicplayer.components.globals.EmptyScreen
 import com.byrondev.musicplayer.viewModels.MusicViewModels
 
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
 fun ArtistScreen (musicViewModels: MusicViewModels, navController: NavController) {
 
-//    val listArtist = musicViewModels.artists.collectAsState().value Todo update this
+    val listArtist = musicViewModels.artists.collectAsState()
+
+    LaunchedEffect(Unit) {
+        musicViewModels.getAllArtists()
+    }
 
     Column (modifier = Modifier.background(color = Color.Black).fillMaxSize()){
-
-        LazyVerticalGrid  (
-            columns =   GridCells.Adaptive(170.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-
-            // Todo Change this
-            items(20) { artist ->
-//                 ArtistCard(artist, navController)
+        if(listArtist.value.isNotEmpty()){
+            LazyVerticalGrid  (
+                columns =   GridCells.Adaptive(170.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(listArtist.value) { artist ->
+                    ArtistCard(artist, navController)
+                }
             }
+        }
+        else {
+            EmptyScreen("Artists empty")
         }
     }
 
