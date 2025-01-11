de0dc7cecd5826ec0273ef6001fc1e9e2371af24
byron <1byron1999@gmail.com>
2025-01-10 16:24:14 -0800

    Add Playlist model/table

--- app/src/main/java/com/byrondev/musicplayer/data/models/Playlist.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/models/Playlist.kt
@@ -1,2 +1,25 @@
 package com.byrondev.musicplayer.data.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName =  "playlist")
+data class Playlist(
+
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+
+    @ColumnInfo(name = "name")
+    val name : String = ""
+)
+
+//PlaylistSongCrossRef table intermedie
+@Entity(
+    primaryKeys = ["playlistId", "songId"],
+    tableName = "playlist_song_cross_ref"
+)
+data class PlaylistSongCrossRef (
+    val playlistId: Int,
+    val songId: Int
+)
