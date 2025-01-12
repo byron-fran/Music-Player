14d8ca6ed3c047e23ba4cb5200429dd8c5191de1
byron <1byron1999@gmail.com>
2025-01-11 19:37:16 -0800

    add list genres into Genres Screen

--- app/src/main/java/com/byrondev/musicplayer/views/genres/GenresScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/genres/GenresScreen.kt
@@ -1,19 +1,31 @@
 package com.byrondev.musicplayer.views.genres
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.byrondev.musicplayer.components.GenreCard
 import com.byrondev.musicplayer.viewModels.MusicViewModels
 
+@RequiresApi(Build.VERSION_CODES.S)
 @Composable
 fun GenresScreen(musicViewModels: MusicViewModels, navController: NavController) {
+
+    val listGenres = musicViewModels.genres.collectAsState()
+
+    LaunchedEffect(Unit) {
+        musicViewModels.getAllGenres()
+    }
 
     LazyVerticalGrid(
         columns = GridCells.Adaptive(170.dp),
@@ -22,8 +34,8 @@ fun GenresScreen(musicViewModels: MusicViewModels, navController: NavController)
         horizontalArrangement = Arrangement.spacedBy(16.dp),
         contentPadding = PaddingValues(16.dp),
     ) {
-        items(13) {
-            GenreCard()
+        items(listGenres.value) { genre ->
+            GenreCard(genre, navController)
         }
     }
 
