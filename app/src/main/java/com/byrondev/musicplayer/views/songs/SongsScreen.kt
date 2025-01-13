85f055b104fb861f7c9b8248edf8b89f492f8609
byron <1byron1999@gmail.com>
2025-01-12 16:45:32 -0800

    Remove LaunchedEffect in SongsScreen

--- app/src/main/java/com/byrondev/musicplayer/views/songs/SongsScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/songs/SongsScreen.kt
@@ -4,7 +4,6 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -15,12 +14,8 @@ import com.byrondev.musicplayer.viewModels.PlayerViewModels
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
 fun SongsScreen (musicViewModels: MusicViewModels, navController: NavController,playerViewModels : PlayerViewModels ) {
+    val listSong = musicViewModels.songs.collectAsState()
 
-    LaunchedEffect(Unit) {
-        musicViewModels.getAllSongs()
-    }
-
-    val listSong = musicViewModels.songs.collectAsState() // Todo update this
     SongList(listSong.value, showTrackNumber = false, PaddingValues(0.dp), playerViewModels)
 
 }
