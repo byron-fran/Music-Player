bb0f5736f977475d6116584928fdd0b9f020f2cc
byron <1byron1999@gmail.com>
2025-01-12 16:49:17 -0800

    Add PlaylistItem component

---
+++ app/src/main/java/com/byrondev/musicplayer/components/playlist/PlaylistItem.kt
@@ -0,0 +1,45 @@
+package com.byrondev.musicplayer.components.playlist
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.byrondev.musicplayer.R
+import com.byrondev.musicplayer.data.models.Playlist
+import com.byrondev.musicplayer.ui.theme.Slate80
+
+@Composable
+fun PlaylistItem(playlist: Playlist) {
+
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Box (
+            modifier = Modifier
+                .width(60.dp)
+                .height(60.dp)
+                .background(Slate80, shape = RoundedCornerShape(5.dp)),
+            contentAlignment = Alignment.Center
+
+        ) {
+            // Todo replace image of playlist if has it
+            Image(
+                painter = painterResource(id = R.drawable.baseline_music_note_600),
+                contentDescription = "Icon",
+                modifier = Modifier.size(30.dp)
+            )
+        }
+        PlaylistInfo(playlist)
+    }
+}
