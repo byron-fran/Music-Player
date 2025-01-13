6bcee2690fefe92f98f0e6ad46aaa6b4dc143864
byron <1byron1999@gmail.com>
2025-01-12 16:44:40 -0800

    Change styles, modifier in text row separation component

--- app/src/main/java/com/byrondev/musicplayer/components/texts/TextRowSeparation.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/texts/TextRowSeparation.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
@@ -13,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.byrondev.musicplayer.components.globals.TextLarge
 import com.byrondev.musicplayer.components.globals.TextMedium
 import com.byrondev.musicplayer.ui.theme.Zinc40
@@ -23,20 +21,18 @@ import com.byrondev.musicplayer.ui.theme.Zinc40
 fun TextRowSeparation(
     text1 : String,
     text2 : String,
-    navController: NavController,
-    path : String
+    modifier: Modifier = Modifier,
+    onClick : () -> Unit
     ) {
     Row (
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top=40.dp, start = 20.dp, end = 20.dp, bottom = 20.dp),
+        modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ){
         TextLarge(text1)
         Row (
             modifier = Modifier, verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-            TextMedium(text2, color = Zinc40, modifier = Modifier.clickable { navController.navigate(path) })
+            TextMedium(text2, color = Zinc40, modifier = Modifier.clickable { onClick() },)
             Icon(
                 imageVector = Icons.AutoMirrored.Default.ArrowForward,
                 modifier = Modifier.size(30.dp),
