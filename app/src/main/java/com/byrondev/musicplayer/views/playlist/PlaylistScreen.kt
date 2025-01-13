ac3cb56c714edb4abe10f3e75463dadc478b58b7
byron <1byron1999@gmail.com>
2025-01-12 16:48:33 -0800

    Modifier playlist layout

--- app/src/main/java/com/byrondev/musicplayer/views/playlist/PlaylistScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/playlist/PlaylistScreen.kt
@@ -1,38 +1,38 @@
 package com.byrondev.musicplayer.views.playlist
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.byrondev.musicplayer.components.playlist.PlaylistCard
+import com.byrondev.musicplayer.components.playlist.PlaylistItem
 import com.byrondev.musicplayer.viewModels.MusicViewModels
 
 
+@RequiresApi(Build.VERSION_CODES.S)
 @Composable
 fun PlaylistScreen(musicViewModels: MusicViewModels, navController: NavController) {
 
+    val playlists = musicViewModels.playlist.collectAsState()
 
     Column (modifier = Modifier.background(color = Color.Black).fillMaxSize()){
-
-        LazyVerticalGrid  (
-            columns = GridCells.Adaptive(170.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            contentPadding = PaddingValues(16.dp),
-
-
-            ) {
-                items(12) {
-                    PlaylistCard()
-                }
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier.offset(x = 0.dp, y = 10.dp)
+        ) {
+            items(playlists.value) {
+                PlaylistItem(it)
+            }
         }
     }
 }
