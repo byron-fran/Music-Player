27b32e3b66ebcad832d04b489eb8dd827c2b791e
byron <1byron1999@gmail.com>
2025-01-12 16:57:31 -0800

    Remove unused code in FavoritesScreen

--- app/src/main/java/com/byrondev/musicplayer/views/FavoritesScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/FavoritesScreen.kt
@@ -2,11 +2,9 @@ package com.byrondev.musicplayer.views
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.byrondev.musicplayer.components.BottomBar
 import com.byrondev.musicplayer.components.songs.SongList
@@ -24,7 +22,6 @@ fun FavoritesScreen(
     )   {
     val songsFavorite = musicViewModels.songs.collectAsState().value.filter { it.isFavorite }
 
-   SongList(songsFavorite,false,PaddingValues(0.dp), playerViewModels )
     Scaffold (
         topBar = { CenterTopAppBar("Favorites") {navController.popBackStack()} },
         bottomBar = { BottomBar(navController) }
