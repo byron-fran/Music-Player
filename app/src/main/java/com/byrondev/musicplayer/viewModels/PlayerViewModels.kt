d5e84dba2360fef1dd0c4a048dda944fda89ce0a
byron <1byron1999@gmail.com>
2025-01-13 21:11:21 -0800

    Add PlaylistDetailScreen route in NavController

--- app/src/main/java/com/byrondev/musicplayer/viewModels/PlayerViewModels.kt
+++ app/src/main/java/com/byrondev/musicplayer/viewModels/PlayerViewModels.kt
@@ -6,7 +6,9 @@ import androidx.lifecycle.viewModelScope
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
+import com.byrondev.musicplayer.data.models.PlaybackQueue
 import com.byrondev.musicplayer.data.models.Song
+import com.byrondev.musicplayer.data.repositories.MusicRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,17 +16,20 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
 @HiltViewModel
-class PlayerViewModels @Inject constructor() : ViewModel(){
+class PlayerViewModels @Inject constructor(
+    private val repository: MusicRepository,
+
+) : ViewModel(){
 
     private val _currentSong = MutableStateFlow(Song())
     val currentSong : StateFlow<Song> = _currentSong
     private val _currentPosition = MutableStateFlow(0L)
     val currentPosition : StateFlow<Long> = _currentPosition
 
-    private val _indexSong = MutableStateFlow(0)
-    val indexSong : StateFlow<Int> = _indexSong
-
+    private val _currentIndexSong = MutableStateFlow(0)
+    val currentIndexSong : StateFlow<Int> = _currentIndexSong
 
     private val _duration = MutableStateFlow(0L)
     val duration : StateFlow<Long> = _duration
@@ -40,37 +45,43 @@ class PlayerViewModels @Inject constructor() : ViewModel(){
     private val _isPlaying = MutableStateFlow(false)
     val isPlaying  = _isPlaying.asStateFlow()
 
-    private val _currentPlaylist = MutableStateFlow<List<MediaItem>>(emptyList())
-    val currentPlaylist: StateFlow<List<MediaItem>> get() = _currentPlaylist
+    private val _currentPlaylist = MutableStateFlow<List<PlaybackQueue>>(emptyList())
+    val currentPlaylist: StateFlow<List<PlaybackQueue>> get() = _currentPlaylist
 
-    fun playByMediaItem(song: Song, index: Int) {
-        println("----------index $index")
-            println("-------player now ${player.toString()}")
-//        _player?.setMediaItem(MediaItem.fromUri(song.uri))
-//        _player?.play()
-        _player?.seekTo(index, 0)
-        _player?.play()
-        _isPlaying.value = true
-        _currentSong.value = song
 
+    init {
+        viewModelScope.launch {
+            repository.getCurrentPlaybackQueue().collect{ items ->
+                _currentPlaylist.value = items
+            }
+        }
     }
+
 
     fun playNextItem () {
         println("---currentMediaItem${player?.currentMediaItem}")
 
         println("---arist${player?.mediaMetadata?.artist}")
 
-        _indexSong.value += 1
-        _currentSong.value = _listSong.value[indexSong.value]
+        _currentIndexSong.value += 1
+        _currentSong.value = _listSong.value[_currentIndexSong.value]
         _player?.seekToNextMediaItem() 
 
     }
 
+    fun playSeekTo(index : Int) {
+        _player?.seekTo(index, 0)
+        _currentIndexSong.value = index
+        _currentSong.value = listSong.value[index]
+        _player?.play()
+        _isPlaying.value = true
+    }
+
     fun playSeekToPreviousMediaItem (  ) {
-        if(indexSong.value < 0) return
+        if(  _currentIndexSong.value < 0) return
         else {
-            _indexSong.value -= 1
-            _currentSong.value = _listSong.value[indexSong.value]
+            _currentIndexSong.value -= 1
+            _currentSong.value = _listSong.value[_currentIndexSong.value]
             _player?.seekToPreviousMediaItem()
         }
     }
@@ -121,37 +132,37 @@ class PlayerViewModels @Inject constructor() : ViewModel(){
         println(" -----Se acabo" )
 
     }
+
     fun loadPlaylist(songs: List<Song>) {
         _listSong.value = songs
 
         val songsUri = songs.map{ it.uri}
 
-        viewModelScope
-            .launch {
-            val mediaItems = songsUri.mapNotNull { uri ->
-                runCatching { MediaItem.fromUri(uri) }.getOrNull()
-            }
-            println("---------mediaItems $mediaItems")
-            if (_currentPlaylist.value != mediaItems) {
-                _currentPlaylist.value = mediaItems
-                _player?.apply {
-                    try {
-                        stop()
-                        clearMediaItems()
-                        setMediaItems(mediaItems)
-                        prepare()
-                    } catch (e: Exception) {
-                        Log.e("PlayerError", "Error al preparar la lista: ${e.message}")
-                    }
+       val uris = songs.map { it -> PlaybackQueue(uri = it.uri) }
+        viewModelScope.launch {
+            val mediaItems = songsUri.mapNotNull { uri -> runCatching { MediaItem.fromUri(uri) }.getOrNull() }
+
+            repository.insertUriToPlaybackQueue(uris)
+            _player?.apply {
+                try {
+                    stop()
+                    clearMediaItems()
+                    setMediaItems(mediaItems)
+                    prepare()
+                } catch (e: Exception) {
+                    Log.e("PlayerError", "Error al preparar la lista: ${e.message}")
                 }
             }
         }
+
     }
 
     fun play() {
-        _currentSong.value = listSong.value[0]
-        _player?.play()
-        _isPlaying.value = true
+        if(listSong.value.isNotEmpty()){
+            _currentSong.value = listSong.value[0]
+            _player?.play()
+            _isPlaying.value = true
+        }
         if(player?.currentPosition != null) {
             _currentPosition.value = player.currentPosition
         }
@@ -170,5 +181,7 @@ class PlayerViewModels @Inject constructor() : ViewModel(){
         player?.seekTo(sliderPosition)
 
     }
+
+
 
 }
