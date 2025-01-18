f42a07e7e9ab7894ae0a9085fd16065fa33decf3
byron <1byron1999@gmail.com>
2025-01-17 21:04:29 -0800

    Add content in PlaylistScreen

--- app/src/main/java/com/byrondev/musicplayer/views/playlist/PlaylistScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/playlist/PlaylistScreen.kt
@@ -2,37 +2,25 @@ package com.byrondev.musicplayer.views.playlist
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.byrondev.musicplayer.components.playlist.PlaylistItem
+import com.byrondev.musicplayer.components.BottomBar
+import com.byrondev.musicplayer.components.playlist.PlaylistItems
+import com.byrondev.musicplayer.components.topbar.CenterTopAppBar
 import com.byrondev.musicplayer.viewModels.MusicViewModels
 
 
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
-fun PlaylistScreen(musicViewModels: MusicViewModels, navController: NavController) {
+fun PlaylistScreen(musicViewModels: MusicViewModels, navController: NavController, songId : Int = 0) {
 
-    val playlists = musicViewModels.playlist.collectAsState()
-
-    Column (modifier = Modifier.background(color = Color.Black).fillMaxSize()){
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(10.dp),
-            modifier = Modifier.offset(x = 0.dp, y = 10.dp)
-        ) {
-            items(playlists.value) {
-                PlaylistItem(it){navController.navigate("PlaylistDetailScreen/${it.id}")}
-            }
-        }
+    Scaffold (
+        topBar = { CenterTopAppBar("Playlists"){navController.popBackStack()} },
+        bottomBar = { BottomBar(navController) },
+        containerColor = Color.Black
+    ) { paddingValues ->
+        PlaylistItems(musicViewModels, navController, paddingValues, songId)
     }
 }
