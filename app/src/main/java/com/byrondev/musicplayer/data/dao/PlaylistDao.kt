7ea9ac29d0555d36f5258bc41c4aec7507a78231
byron <1byron1999@gmail.com>
2025-01-17 21:01:27 -0800

    Update many changes in PlaylistDao

--- app/src/main/java/com/byrondev/musicplayer/data/dao/PlaylistDao.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/dao/PlaylistDao.kt
@@ -2,11 +2,11 @@ package com.byrondev.musicplayer.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
 import com.byrondev.musicplayer.data.models.Playlist
 import com.byrondev.musicplayer.data.models.PlaylistSongCrossRef
-import com.byrondev.musicplayer.data.models.Song
 import com.byrondev.musicplayer.data.relations.PlaylistWithSongs
 import kotlinx.coroutines.flow.Flow
 
@@ -16,16 +16,31 @@ interface PlaylistDao {
     @Insert
     suspend fun insertPlaylist(playlist: Playlist): Long
 
-    @Insert
-    suspend fun insertSong(song: Song): Long
-
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertPlaylistSongCrossRef(crossRef: PlaylistSongCrossRef)
 
     @Transaction
-    @Query("SELECT * FROM playlist WHERE id = :playlistId")
-    suspend fun getPlaylistWithSongs(playlistId: Int): PlaylistWithSongs
+    @Query("""
+        SELECT * FROM playlist WHERE id = :playlistId
+    """)
+    fun getPlaylistWithSongs(playlistId: Int): Flow<List<PlaylistWithSongs>>
 
-    @Query("SELECT * FROM playlist")
-    fun getAllPlaylists() :Flow <List<Playlist>>
+
+    @Query(""" 
+        SELECT p.id, p.name, 
+            COUNT(ps.songId) AS songCount,
+            COALESCE(SUM(s.duration), 0) AS duration
+        FROM playlist p
+        LEFT JOIN playlist_song_cross_ref ps ON p.id = ps.playlistId
+        LEFT JOIN songs s ON ps.songId = s.id
+        GROUP BY p.id
+    """)
+    fun getAllPlaylists() :Flow <List<PlaylistWithCountSong>>
 }
+
+data class PlaylistWithCountSong(
+    val id : Int = 0,
+    val name : String = "",
+    val songCount : Int = 0,
+    val duration : Int = 0
+)
