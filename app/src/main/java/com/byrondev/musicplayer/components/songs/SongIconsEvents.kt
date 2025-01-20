9e1de2ba9e72761e02107bc7f969f621f1b892d0
byron <1byron1999@gmail.com>
2025-01-19 20:24:51 -0800

    Update layout fonts, icons in SongIconsEvents

--- app/src/main/java/com/byrondev/musicplayer/components/songs/SongIconsEvents.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/songs/SongIconsEvents.kt
@@ -1,54 +1,74 @@
 package com.byrondev.musicplayer.components.songs
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.byrondev.musicplayer.R
+import com.byrondev.musicplayer.components.globals.TextExtraSmall
+import com.byrondev.musicplayer.components.globals.TextLarge
 import com.byrondev.musicplayer.data.models.Song
 import com.byrondev.musicplayer.viewModels.MusicViewModels
-import com.byrondev.musicplayer.viewModels.PlayerViewModels
 
+@RequiresApi(Build.VERSION_CODES.S)
 @Composable
-fun SongIconsEvents(song: Song, playerViewModels: PlayerViewModels) {
+fun SongIconsEvents(
+    song: Song,
+    musicViewModels: MusicViewModels,
+    navController: NavController,
+    ) {
 
-    Column {
+    val isFavorite = remember { mutableStateOf(song.isFavorite) }
+
+    Column (
+        modifier = Modifier.offset(x= 0.dp, y =30.dp)
+    ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
             Icon(
-                painter = painterResource(id = R.drawable.playlist_add),
+                painter = painterResource(id = R.drawable.baseline_playlist_30),
+                modifier = Modifier.size(30.dp).clickable { navController.navigate("PlaylistScreen/${song.id}") },
                 contentDescription = "",
-                modifier = Modifier.size(30.dp),
                 tint = Color.White
             )
-            if (song.bitRate != 0) {
-                Text(
-                    "FLAC ${song.bitRate.toString().substring(0, 4)} Kbps",
-                    color = Color.White
-                )
+            Column (
+               horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(5.dp)
+            ) {
+                TextLarge(text = song.title ?: "", modifier = Modifier.width(300.dp))
+                TextExtraSmall(text=song.album ?: "Album unknown",modifier = Modifier.width(300.dp))
             }
             Icon(
-                imageVector = Icons.Default.Favorite,
+                imageVector = if (isFavorite.value)  Icons.Default.Favorite else Icons.Default.FavoriteBorder,
                 contentDescription = "",
-                modifier = Modifier.size(30.dp),
+                modifier = Modifier.size(30.dp).clickable {
+                    isFavorite.value = !isFavorite.value
+                    musicViewModels.updateIsFavoriteSong(song.id, isFavorite.value)
+                },
                 tint = Color.White
             )
         }
-
     }
 }
