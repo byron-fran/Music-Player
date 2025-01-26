62b25c095d001b39d3b42d0c04d022b06067243b
byron <1byron1999@gmail.com>
2025-01-25 16:57:40 -0800

    Update format and add size default in CoverImage

--- app/src/main/java/com/byrondev/musicplayer/components/images/CoverImage.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/images/CoverImage.kt
@@ -3,18 +3,26 @@ package com.byrondev.musicplayer.components.images
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.byrondev.musicplayer.R
 import com.byrondev.musicplayer.utils.decodeBitmapWithSubsampling
 
 
 @Composable
-fun CoverImage(byteArray: ByteArray?, modifier: Modifier = Modifier, contentScale: ContentScale = ContentScale.Crop) {
+fun CoverImage(
+    byteArray: ByteArray?,
+    modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.Crop,
+    sizeDefaultCover : Dp = 40.dp
+    ) {
 
     val imageBitmap = byteArray?.let{ decodeBitmapWithSubsampling(byteArray, 600, 600)}
 
@@ -30,6 +38,7 @@ fun CoverImage(byteArray: ByteArray?, modifier: Modifier = Modifier, contentScal
     else {
         Box(modifier= modifier, contentAlignment = Alignment.TopStart,) {
             Image(
+                modifier = Modifier.size(sizeDefaultCover),
                 painter = painterResource(id= R.drawable.baseline_music_note_600),
                 contentDescription = "Cover Image default",
                 )
