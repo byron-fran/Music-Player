833b0ecee0b66fc68be16ba9554176cfb73e05ba
byron <1byron1999@gmail.com>
2025-02-02 04:00:27 -0800

    Update Layout Album

--- app/src/main/java/com/byrondev/musicplayer/components/albums/AlbumCard.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/albums/AlbumCard.kt
@@ -1,10 +1,14 @@
 package com.byrondev.musicplayer.components.albums
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,50 +18,65 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.byrondev.musicplayer.R
 import com.byrondev.musicplayer.components.globals.CircleSeparation
+import com.byrondev.musicplayer.components.globals.LazyImageCover
 import com.byrondev.musicplayer.components.globals.TextExtraSmall
 import com.byrondev.musicplayer.components.globals.TextMedium
-import com.byrondev.musicplayer.components.images.CoverImage
-import com.byrondev.musicplayer.data.models.Album
+import com.byrondev.musicplayer.data.dao.AlbumResponse
+import com.byrondev.musicplayer.utils.dates.parseYear
 
 
+@RequiresApi(Build.VERSION_CODES.S)
 @Composable
-fun AlbumCard(album: Album, navController: NavController, modifier: Modifier = Modifier) {
+fun AlbumCard(
+    album: AlbumResponse,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    ) {
+
 
     Card(
-        modifier = modifier
-            .clickable { navController.navigate("AlbumDetail/${album.id}") }
-            .fillMaxWidth(),
+        modifier = modifier.clickable { navController.navigate("AlbumDetail/${album.id}") },
         colors = CardDefaults.cardColors(containerColor = Color.Black),
         shape = RoundedCornerShape(0.dp)
     ) {
-        CoverImage(album.cover, contentScale = ContentScale.Crop, modifier = Modifier.clip(RoundedCornerShape(5.dp)))
+        LazyImageCover(
+            album.cover,
+            painterResource(id = R.drawable.image_cd_default),
+            modifier = Modifier.aspectRatio(1f)
+        )
         Row(
-            modifier = modifier.padding(top = 5.dp).fillMaxWidth(),
+            modifier = modifier
+                .padding(top = 5.dp)
+                .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Column(modifier = modifier.weight(1f) ) {
-                TextMedium(album.title ?: "Album unknown")
-                Row (
+            Column(
+                modifier = modifier
+                    .weight(1f)
+                    .background(Color.Black),
+                verticalArrangement = Arrangement.spacedBy(2.dp),
+            ) {
+                TextMedium(album.title)
+                Row(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.spacedBy(10.dp)
                 ) {
-                    TextExtraSmall(text=album.artist ?: "")
-                    if(album.year != null) {
+                    val year = parseYear(album.year)
+                    TextExtraSmall(text = album.artist)
+                    if (year != null) {
                         CircleSeparation()
-                        TextExtraSmall(text=album.year.substringBefore("-").substringBefore(";").substringBefore("/"))
+                        TextExtraSmall(text = year.toString())
                     }
                 }
             }
-            album.quality?.let {
-                if(it > 0) {
+            album.numOfHiresQuality.let {
+                if (it > 0) {
                     Image(
                         painter = painterResource(id = R.drawable.hi_res_logo),
                         modifier = Modifier.size(25.dp),
