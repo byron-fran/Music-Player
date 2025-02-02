287abd476cafc6a474ca1dd80cc01d2743ca748e
byron <1byron1999@gmail.com>
2025-02-02 05:33:07 -0800

    Update image default in CoverImage

--- app/src/main/java/com/byrondev/musicplayer/components/images/CoverImage.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/images/CoverImage.kt
@@ -1,16 +1,13 @@
 package com.byrondev.musicplayer.components.images
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.byrondev.musicplayer.R
 import com.byrondev.musicplayer.utils.decodeBitmapWithSubsampling
@@ -21,27 +18,26 @@ fun CoverImage(
     byteArray: ByteArray?,
     modifier: Modifier = Modifier,
     contentScale: ContentScale = ContentScale.Crop,
-    sizeDefaultCover : Dp = 40.dp
-    ) {
+) {
 
-    val imageBitmap = byteArray?.let{ decodeBitmapWithSubsampling(byteArray, 600, 600)}
+    val imageBitmap = byteArray?.let { decodeBitmapWithSubsampling(byteArray, 600, 600) }
 
     if (imageBitmap != null) {
         Image(
-            modifier = modifier.fillMaxWidth(),
+            modifier = modifier
+                .clip(shape = RoundedCornerShape(5.dp)),
             bitmap = imageBitmap.asImageBitmap(),
             contentDescription = "Cover Image",
             contentScale = contentScale
 
         )
-        }
-    else {
-        Box(modifier= modifier, contentAlignment = Alignment.TopStart,) {
-            Image(
-                modifier = Modifier.size(sizeDefaultCover),
-                painter = painterResource(id= R.drawable.baseline_music_note_600),
-                contentDescription = "Cover Image default",
-                )
-        }
+    } else {
+        Image(
+            modifier = modifier
+                .clip(shape = RoundedCornerShape(5.dp)),
+            painter = painterResource(id = R.drawable.image_music_default),
+            contentDescription = "Cover Image default",
+        )
+
     }
 }
