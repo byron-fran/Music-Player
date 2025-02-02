11f03ccade625e7af81eea33bb439c47cb4c5e32
byron <1byron1999@gmail.com>
2025-02-02 05:16:53 -0800

    Update layout SongList

--- app/src/main/java/com/byrondev/musicplayer/components/songs/SongList.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/songs/SongList.kt
@@ -2,21 +2,18 @@ package com.byrondev.musicplayer.components.songs
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewModelScope
@@ -34,44 +31,45 @@ import kotlinx.coroutines.launch
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
 fun SongList(
-    songs : List<Song>,
-    showTrackNumber : Boolean = true,
+    songs: List<Song>,
+    showTrackNumber: Boolean = true,
     paddingValues: PaddingValues,
-    playerViewModels : PlayerViewModels,
-    navController: NavController
-    ){
+    playerViewModels: PlayerViewModels,
+    navController: NavController,
+) {
 
     LaunchedEffect(songs.size) {
         playerViewModels.updateCurrentListSongs(songs)
     }
 
-    Column (modifier = Modifier.background(color = Color.Black).fillMaxSize().padding(paddingValues)){
-        if(songs.isNotEmpty()) {
-            LazyColumn  (
-                modifier = Modifier.padding(top = 10.dp),
-                verticalArrangement = Arrangement.spacedBy(5.dp),
-                ) {
-                item {
-                    ButtonsPlayAlbum(playerViewModels, songs.count(), modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp))
-                }
-                itemsIndexed(songs) {index, song ->
-                    Row  (verticalAlignment = Alignment.CenterVertically) {
-                        LazyImageCover(
-                            playerViewModels, song.uri,
-                            painterResource(R.drawable.baseline_music_note_600),
-                            modifier = Modifier.size(50.dp)
-                        )
-                        SongCard (song,showTrackNumber, navController) {
-                            playerViewModels.viewModelScope.launch {
-                                playerViewModels.playSeekTo(index)
-                            }
+    if (songs.isNotEmpty()) {
+        LazyColumn(
+            modifier = Modifier.padding(top = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+        ) {
+            item {
+                ButtonsPlayAlbum(
+                    playerViewModels,
+                    songs.count(),
+                    modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)
+                )
+            }
+            itemsIndexed(songs) { index, song ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    LazyImageCover(
+                        uri = song.uri,
+                        painterResource(R.drawable.image_music_default),
+                        modifier = Modifier.width(50.dp).aspectRatio(1f)
+                    )
+                    SongCard(song, showTrackNumber, navController) {
+                        playerViewModels.viewModelScope.launch {
+                            playerViewModels.playSeekTo(index)
                         }
                     }
                 }
             }
         }
-        else {
-            EmptyScreen(){ TextLarge("Songs empty", color= Zinc40) }
-        }
+    } else {
+        EmptyScreen() { TextLarge("Songs empty", color = Zinc40) }
     }
 }
