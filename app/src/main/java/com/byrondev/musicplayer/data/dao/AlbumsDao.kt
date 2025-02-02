9db1493ca8c47fa5eac27fb772009e68e7ba0a4a
byron <1byron1999@gmail.com>
2025-02-02 05:42:32 -0800

    Update query to get Album with songs in AlbumsDao

--- app/src/main/java/com/byrondev/musicplayer/data/dao/AlbumsDao.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/dao/AlbumsDao.kt
@@ -4,24 +4,19 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.RewriteQueriesToDropUnusedColumns
 import androidx.room.Transaction
+import androidx.room.Update
 import com.byrondev.musicplayer.data.models.Album
 import com.byrondev.musicplayer.data.relations.AlbumWithSongs
 import kotlinx.coroutines.flow.Flow
 
 @Dao
+@RewriteQueriesToDropUnusedColumns
 interface AlbumsDao {
 
-    @Query("""
-        SELECT
-            a.id as id,a.title as title,a.album_artist as artist,
-            a.year as year,a.cover as cover,
-        COUNT(CASE WHEN s.bit_rate >= 24 THEN 1 ELSE NULL END) as quality
-        FROM albums a
-        LEFT JOIN songs s ON s.album_id = a.id
-        GROUP BY a.id ORDER BY year DESC
-    """)
-    fun getAllAlbums() : Flow<List<Album>>
+    @Query("SELECT * FROM AlbumResponse")
+    fun getAllAlbums() : Flow<List<AlbumResponse>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAlbum(album: Album) : Long
@@ -30,6 +25,18 @@ interface AlbumsDao {
     suspend fun getAlbumByTitle(title : String, albumArtist : String) : Album?
 
     @Transaction
-    @Query("SELECT * FROM albums WHERE id = :id")
+    @Query(""" 
+      SELECT
+        a.title,  a.year, a.album_artist as artist, a.id, a.copyright, a.genres, s.uri as cover, a.artist_id,
+        a.album_artist, a.release_date, a.num_of_hires_quality, a.duration, a.tracks_total, a.num_tracks,a.created_at,
+      COUNT(s.album_id = a.id)  as numTracks, CASE WHEN af.album_id NOT NULL THEN 1 ELSE 0 END as is_favorite FROM albums a
+      LEFT JOIN songs s ON s.album_id = a.id
+      LEFT JOIN albums_favorite af ON af.album_id = a.id
+      WHERE a.id=:id
+      """)
     fun getAlbumWithSongs(id: Int): Flow<AlbumWithSongs>
+
+    @Update
+    suspend fun updateIsFavoriteAlbum(album: Album)
 }
+
