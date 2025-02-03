2fc51df80f18460a1fa7133229b3e99f8819d0de
byron <1byron1999@gmail.com>
2025-02-03 05:54:05 -0800

    Add albums carousel in FavoritesScreen

--- app/src/main/java/com/byrondev/musicplayer/views/FavoritesScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/FavoritesScreen.kt
@@ -2,10 +2,16 @@ package com.byrondev.musicplayer.views
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.byrondev.musicplayer.components.albums.AlbumsCarousel
 import com.byrondev.musicplayer.components.songs.SongList
 import com.byrondev.musicplayer.components.topbar.CenterTopAppBar
 import com.byrondev.musicplayer.viewModels.MusicViewModels
@@ -20,10 +26,20 @@ fun FavoritesScreen(
 
     )   {
     val songsFavorite = musicViewModels.songs.collectAsState().value.filter { it.isFavorite }
+    val albumsFavorite = musicViewModels.albumsFavorites.collectAsState()
 
-    Scaffold (
-        topBar = { CenterTopAppBar("Favorites") {navController.popBackStack()} },
-    ) { paddingValues ->
-        SongList(songsFavorite,false,paddingValues, playerViewModels, navController )
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+            CenterTopAppBar(title = "Favorites") { }
+            AlbumsCarousel(
+                albumsFavorite.value,
+                navController,
+                text1 = "Albumes Favoritos",
+                text2 = "",
+            ) { }
+        SongList(songsFavorite,false, PaddingValues(0.dp), playerViewModels, navController )
+       }
     }
 }
