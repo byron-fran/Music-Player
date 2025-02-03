569983f893fe45560166da9e684155f62698bd26
byron <1byron1999@gmail.com>
2025-02-03 05:19:36 -0800

    Update query to get album by id in AlbumsDao

--- app/src/main/java/com/byrondev/musicplayer/data/dao/AlbumsDao.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/dao/AlbumsDao.kt
@@ -16,24 +16,29 @@ import kotlinx.coroutines.flow.Flow
 interface AlbumsDao {
 
     @Query("SELECT * FROM AlbumResponse")
-    fun getAllAlbums() : Flow<List<AlbumResponse>>
+    fun getAllAlbums(): Flow<List<AlbumResponse>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAlbum(album: Album) : Long
+    suspend fun insertAlbum(album: Album): Long
 
     @Query("SELECT * FROM albums WHERE title=:title AND artist = :albumArtist LIMIT 1")
-    suspend fun getAlbumByTitle(title : String, albumArtist : String) : Album?
+    suspend fun getAlbumByTitle(title: String, albumArtist: String): Album?
 
     @Transaction
-    @Query(""" 
-      SELECT
+    @Query(
+        """ 
+        SELECT
         a.title,  a.year, a.album_artist as artist, a.id, a.copyright, a.genres, s.uri as cover, a.artist_id,
-        a.album_artist, a.release_date, a.num_of_hires_quality, a.duration, a.tracks_total, a.num_tracks,a.created_at,
-      COUNT(s.album_id = a.id)  as numTracks, CASE WHEN af.album_id NOT NULL THEN 1 ELSE 0 END as is_favorite FROM albums a
-      LEFT JOIN songs s ON s.album_id = a.id
-      LEFT JOIN albums_favorite af ON af.album_id = a.id
-      WHERE a.id=:id
-      """)
+        a.album_artist, a.release_date, a.num_of_hires_quality, a.tracks_total,a.created_at,
+        COUNT(s.album_id)  as num_tracks,
+        SUM(s.duration) as duration,
+        CASE WHEN af.album_id NOT NULL THEN 1 ELSE 0 END as is_favorite 
+        FROM albums a
+        LEFT JOIN songs s ON s.album_id = a.id
+        LEFT JOIN albums_favorite af ON af.album_id = a.id
+        WHERE a.id=:id
+      """
+    )
     fun getAlbumWithSongs(id: Int): Flow<AlbumWithSongs>
 
     @Update
