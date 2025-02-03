f58c913015669d574fc3112eb9763698cc5803e0
byron <1byron1999@gmail.com>
2025-02-03 06:04:13 -0800

    Update PlaylistDetailScreen

--- app/src/main/java/com/byrondev/musicplayer/views/playlist/PlaylistDetailScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/playlist/PlaylistDetailScreen.kt
@@ -2,7 +2,6 @@ package com.byrondev.musicplayer.views.playlist
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,14 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.byrondev.musicplayer.components.globals.HeaderContent
+import com.byrondev.musicplayer.components.globals.TopAppBarLeft
 import com.byrondev.musicplayer.components.songs.SongList
 import com.byrondev.musicplayer.utils.dates.formatDuration
 import com.byrondev.musicplayer.viewModels.MusicViewModels
@@ -34,18 +32,11 @@ fun PlaylistDetailScreen(
     playerViewModels: PlayerViewModels,
     id : Int
     ) {
-
     val playlistWithSongs = musicViewModels.playlistWithSongs.collectAsState()
-
     val lazyListState = rememberLazyListState()
-
     val scrollOffset = remember {
-        derivedStateOf { lazyListState.firstVisibleItemScrollOffset > 0 }
+        derivedStateOf { lazyListState.firstVisibleItemIndex > 0 }
     }
-    val targetHeight: Dp by remember {
-        derivedStateOf { if (scrollOffset.value) 80.dp else 370.dp }
-    }
-    val animatedHeight by animateDpAsState(targetValue = targetHeight, label = "",)
 
     LaunchedEffect(id) {
         musicViewModels.getPlaylistWithSongs(id)
@@ -54,16 +45,13 @@ fun PlaylistDetailScreen(
     Box(
         modifier = Modifier.fillMaxSize().background(Color.Black),
     ) {
-        Column (
-            modifier = Modifier.fillMaxSize()
-        ) {
+        Column {
             playlistWithSongs.value.forEach {
+                TopAppBarLeft(it.playlist.name,scrollOffset ) { navController.popBackStack()}
                 val totalDuration = it.songs.sumOf { s -> s.duration }
                 HeaderContent(
                     title = it.playlist.name,
                     texts = listOf("${it.songs.count()} songs", formatDuration(totalDuration.toInt())),
-                    height = animatedHeight,
-                    scrollOffset = scrollOffset,
                     navController =navController,
                     bytesArray = listOf(ByteArray(2))
                 )
