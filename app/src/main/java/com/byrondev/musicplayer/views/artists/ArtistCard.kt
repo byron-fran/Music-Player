26b5378bd8b13899561188d66c7128d02bce9b04
byron <1byron1999@gmail.com>
2025-02-04 10:18:09 -0800

    Update layout of ArtistCard

--- app/src/main/java/com/byrondev/musicplayer/views/artists/ArtistCard.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/artists/ArtistCard.kt
@@ -1,56 +1,84 @@
 package com.byrondev.musicplayer.views.artists
 
-import androidx.compose.foundation.Image
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import com.byrondev.musicplayer.R
+import com.byrondev.musicplayer.components.globals.CircleSeparation
+import com.byrondev.musicplayer.components.globals.LazyImageCover
+import com.byrondev.musicplayer.components.globals.TextExtraSmall
+import com.byrondev.musicplayer.components.globals.TextMedium
 import com.byrondev.musicplayer.data.models.Artist
+import com.byrondev.musicplayer.ui.theme.Slate80
 
+
+@RequiresApi(Build.VERSION_CODES.S)
+@Composable
+fun ArtistCard(artist: Artist, onClick: () -> Unit) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier
+            .clickable { onClick() }
+            .fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier
+                .width(60.dp)
+                .height(60.dp)
+                .background(Slate80, shape = RoundedCornerShape(5.dp)),
+            contentAlignment = Alignment.Center
+
+        ) {
+            LazyImageCover(
+                artist.cover,
+                painterResource(id = R.drawable.artist_50),
+                modifier = Modifier.size(50.dp)
+            )
+        }
+        ArtistCardInfo(artist)
+    }
+}
 
 @Composable
-fun ArtistCard(artist : Artist, navController: NavController) {
+fun ArtistCardInfo(artist: Artist, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.height(40.dp),
+        verticalArrangement = Arrangement.spacedBy(2.dp)
+    ) {
+        TextMedium(artist.name ?: "Arist unknown")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            artist.numOfSongs?.let {
+                var song = "Song"
+                if (it > 1) song += "s"
+                TextExtraSmall(text = "${artist.numOfSongs} $song")
+            }
+            artist.numOfAlbums?.let {
+                CircleSeparation()
+                var text = "Album"
+                if (it > 1) text += "s"
+                TextExtraSmall(text = "$it $text")
+            }
+        }
 
-    Card (
-        shape = CircleShape,
-        colors =  CardDefaults.cardColors(
-            /* Todo add color card*/
-
-        ),
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { navController.navigate("ArtistDetail/${artist.id}") }
-    ){
-        Image(
-            painter = painterResource(id= R.drawable.baseline_person_24),
-            contentDescription =  "",
-            modifier = Modifier.fillMaxWidth(),
-            alignment = Alignment.Center,
-
-        )
-        Text("${artist.name}",
-            color= Color.White,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.width(100.dp).padding(bottom = 10.dp).align(Alignment.CenterHorizontally),
-            maxLines = 2,
-            overflow = TextOverflow.Ellipsis,
-            fontSize = 17.sp
-            )
     }
-
 }
