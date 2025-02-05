e50946aebd45881f0d5467a4bf0fbee3fd65a5af
byron <1byron1999@gmail.com>
2025-02-04 17:18:36 -0800

    Update ArtistsDao query

--- app/src/main/java/com/byrondev/musicplayer/data/dao/ArtistsDao.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/dao/ArtistsDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import com.byrondev.musicplayer.data.databaseViews.AlbumResponse
 import com.byrondev.musicplayer.data.models.Artist
 import com.byrondev.musicplayer.data.relations.ArtistWithSongs
 import kotlinx.coroutines.flow.Flow
@@ -12,7 +13,15 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface ArtistsDao {
 
-    @Query("SELECT * FROM artists ORDER BY name ASC")
+    @Query(""" 
+        SELECT a.id,a.cover,a.name,
+        COUNT(DISTINCT s.id) AS num_of_songs,
+        COUNT(DISTINCT al.id) AS num_of_albums
+        FROM artists a
+        LEFT JOIN albums al ON al.artist_id = a.id
+        LEFT JOIN songs s ON s.album_id = al.id
+        GROUP BY a.name
+      """)
     fun getAllArtist () : Flow<List<Artist>>
 
     @Query("SELECT * FROM artists WHERE name=:name COLLATE NOCASE")
@@ -27,7 +36,9 @@ interface ArtistsDao {
 
     @Query(""" 
         SELECT
-        ar.id, ar.title, ar.artist, ar.year, ar.cover, ar.numOfHiresQuality, ar.artistId
+        ar.id, ar.title, ar.artist, ar.year, 
+        ar.cover, ar.numOfHiresQuality, 
+        ar.artistId,ar.tracksTotal, ar.numTracks
         FROM AlbumResponse ar
         INNER JOIN artists a ON ar.artistId = a.id
         WHERE a.id=:id
