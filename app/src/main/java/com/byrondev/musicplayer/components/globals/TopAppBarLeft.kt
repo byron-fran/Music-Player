3ceedcff15eea901b012a016b243ba9d0ec31c44
byron <1byron1999@gmail.com>
2025-02-04 16:14:44 -0800

    Update  Layout TopAppBarLeft

--- app/src/main/java/com/byrondev/musicplayer/components/globals/TopAppBarLeft.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/globals/TopAppBarLeft.kt
@@ -1,54 +1,65 @@
 package com.byrondev.musicplayer.components.globals
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun  TopAppBarLeft (
+fun TopAppBarLeft(
     title: String,
     scrollOffset: State<Boolean>,
     modifier: Modifier = Modifier,
     color: Color = Color.Transparent,
-    onClick: () -> Unit
-
+    onClick: () -> Unit,
     ) {
-    TopAppBar(
-        modifier = modifier,
-        title = {
-            AnimatedVisibility(visible = scrollOffset.value) {
-                TextLarge(title)
-            }
-        },
-        colors = TopAppBarDefaults.topAppBarColors(containerColor = color),
-        navigationIcon = {
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color)
+            .padding(bottom = 10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
             Icon(
                 imageVector = Icons.AutoMirrored.Default.ArrowBack,
                 contentDescription = "",
-                modifier = Modifier.size(30.dp).clickable { onClick() },
+                modifier = Modifier
+                    .size(25.dp)
+                    .clickable { onClick() },
                 tint = Color.White
             )
-        },
-        actions =  {
-            Icon(
-                imageVector = Icons.Default.MoreVert,
-                contentDescription = "",
-                modifier = Modifier.size(25.dp).clickable { /* Todo add event */ },
-                tint = Color.White
-            )
+            AnimatedVisibility(visible = scrollOffset.value) {
+                TextLarge(title)
+            }
         }
-    )
+        Icon(
+            imageVector = Icons.Default.MoreVert,
+            contentDescription = "",
+            modifier = Modifier
+                .size(25.dp)
+                .clickable { /* Todo add event */ },
+            tint = Color.White
+        )
+    }
 }
