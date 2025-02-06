e990aa2d08cbf5e695edb189eb7a8573994d3d41
byron <1byron1999@gmail.com>
2025-02-05 21:00:14 -0800

    Update queries in select by name and albumsWithSongs

--- app/src/main/java/com/byrondev/musicplayer/data/dao/AlbumsDao.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/dao/AlbumsDao.kt
@@ -7,6 +7,7 @@ import androidx.room.Query
 import androidx.room.RewriteQueriesToDropUnusedColumns
 import androidx.room.Transaction
 import androidx.room.Update
+import com.byrondev.musicplayer.data.databaseViews.AlbumResponse
 import com.byrondev.musicplayer.data.models.Album
 import com.byrondev.musicplayer.data.relations.AlbumWithSongs
 import kotlinx.coroutines.flow.Flow
@@ -21,7 +22,7 @@ interface AlbumsDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAlbum(album: Album): Long
 
-    @Query("SELECT * FROM albums WHERE title=:title AND artist = :albumArtist LIMIT 1")
+    @Query("SELECT * FROM albums a WHERE title=:title COLLATE NOCASE AND artist = :albumArtist LIMIT 1")
     suspend fun getAlbumByTitle(title: String, albumArtist: String): Album?
 
     @Transaction
@@ -29,10 +30,10 @@ interface AlbumsDao {
         """ 
         SELECT
         a.title,  a.year, a.album_artist as artist, a.id, a.copyright, a.genres, s.uri as cover, a.artist_id,
-        a.album_artist, a.release_date, a.num_of_hires_quality, a.tracks_total,a.created_at,
+        a.album_artist, a.release_date, a.tracks_total,a.created_at,
         COUNT(s.album_id)  as num_tracks,
         SUM(s.duration) as duration,
-        CASE WHEN af.album_id NOT NULL THEN 1 ELSE 0 END as is_favorite 
+        CASE WHEN af.album_id NOT NULL THEN 1 ELSE 0 END as is_favorite
         FROM albums a
         LEFT JOIN songs s ON s.album_id = a.id
         LEFT JOIN albums_favorite af ON af.album_id = a.id
@@ -44,4 +45,3 @@ interface AlbumsDao {
     @Update
     suspend fun updateIsFavoriteAlbum(album: Album)
 }
-
