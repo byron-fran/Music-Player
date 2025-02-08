d4152544f92c500ba89e8830ec92b7ef80fd5f40
byron <1byron1999@gmail.com>
2025-02-07 22:56:20 -0800

    Add and remove arguments in TabItem

--- app/src/main/java/com/byrondev/musicplayer/tabs/TabItem.kt
+++ app/src/main/java/com/byrondev/musicplayer/tabs/TabItem.kt
@@ -3,7 +3,11 @@ package com.byrondev.musicplayer.tabs
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.byrondev.musicplayer.R
+import com.byrondev.musicplayer.components.globals.TextMedium
 import com.byrondev.musicplayer.components.playlist.PlaylistItems
 import com.byrondev.musicplayer.viewModels.MusicViewModels
 import com.byrondev.musicplayer.viewModels.PlayerViewModels
@@ -12,18 +16,92 @@ import com.byrondev.musicplayer.views.artists.ArtistScreen
 import com.byrondev.musicplayer.views.genres.GenresScreen
 import com.byrondev.musicplayer.views.songs.SongsScreen
 
-typealias ComposableFun = @Composable ( MusicViewModels, PlayerViewModels, NavController ) -> Unit
+typealias StringFun = @Composable () -> Unit
+typealias ComposableFun = @Composable (MusicViewModels, PlayerViewModels, NavController) -> Unit
 
-sealed class TabItem( var title: String, var screen: ComposableFun) {
+sealed class TabItem(
+    val stringRes: StringFun,
+    var screen: ComposableFun,
+) {
     @RequiresApi(Build.VERSION_CODES.S)
-    data object Albums : TabItem( "Albums", { musicViewModels, playerViewModel, navController -> AlbumsScreen( navController,musicViewModels) } )
-    @RequiresApi(Build.VERSION_CODES.S)
-    data object Songs : TabItem( "Songs", { musicViewModels, playerViewModel, navController ->   SongsScreen(musicViewModels, navController, playerViewModel) })
-    @RequiresApi(Build.VERSION_CODES.S)
-    data object Artists : TabItem( "Artists", { musicViewModels, _, navController ->  ArtistScreen(musicViewModels, navController) })
-    @RequiresApi(Build.VERSION_CODES.S)
-    data object Playlists : TabItem("Playlists", { musicViewModels, _, navController -> PlaylistItems( musicViewModels, navController) })
-    @RequiresApi(Build.VERSION_CODES.S)
-    data object Genres : TabItem("Genres", { musicViewModels, _, navController -> GenresScreen( musicViewModels, navController) })
+    data object Albums : TabItem(
+        stringRes = {
+            TextMedium(
+                stringResource(id = R.string.library_albums),
+                color = Color.Unspecified,
+            )
+        },
+        { musicViewModels, _, navController ->
+            AlbumsScreen(
+                navController,
+                musicViewModels
+            )
+        }
+    )
 
+    @RequiresApi(Build.VERSION_CODES.S)
+    data object Songs : TabItem(
+        stringRes = {
+            TextMedium(
+                text = stringResource(id = R.string.library_songs),
+                color = Color.Unspecified
+            )
+        },
+
+        { musicViewModels, playerViewModel, navController ->
+            SongsScreen(
+                musicViewModels,
+                navController,
+                playerViewModel
+            )
+        }
+    )
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    data object Artists : TabItem(
+        stringRes = {
+            TextMedium(
+                text = stringResource(id = R.string.library_artists),
+                color = Color.Unspecified
+            )
+        },
+
+        { musicViewModels, _, navController ->
+            ArtistScreen(
+                musicViewModels,
+                navController
+            )
+        })
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    data object Playlists : TabItem(
+        stringRes = {
+            TextMedium(
+                text = stringResource(id = R.string.library_playlists),
+                color = Color.Unspecified
+            )
+        },
+        { musicViewModels, _, navController ->
+            PlaylistItems(
+                musicViewModels,
+                navController
+            )
+        }
+    )
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    data object Genres : TabItem(
+        stringRes = {
+            TextMedium(
+                text = stringResource(id = R.string.library_genre),
+                color = Color.Unspecified
+            )
+        },
+        { musicViewModels, _, navController ->
+            GenresScreen(
+                musicViewModels,
+                navController
+            )
+        }
+    )
 }
