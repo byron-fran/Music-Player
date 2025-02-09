d071ea100f34802770957b5ff6bb6cbedb5a4561
byron <1byron1999@gmail.com>
2025-02-09 07:46:45 -0800

    Change Row position in MenuItem

--- app/src/main/java/com/byrondev/musicplayer/components/menu/MenuItem.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/menu/MenuItem.kt
@@ -1,37 +1,32 @@
 package com.byrondev.musicplayer.components.menu
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.byrondev.musicplayer.ui.theme.textWhite15
+import com.byrondev.musicplayer.components.globals.IconSmall
+import com.byrondev.musicplayer.components.globals.TextMedium
 
 @Composable
-fun MenuItem(icon: Painter, size: Dp, title : String ,  ) {
-    Row (
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(10.dp),
-        modifier = Modifier
-            .padding(horizontal = 5.dp, vertical = 7.dp )
-            .clickable { /* !todo add events */ }
-    ) {
-        Icon(
-            painter =  icon,
-            contentDescription = "",
-            tint = Color.White,
-            modifier = Modifier.size(size)
-        )
-        Text(title, style = textWhite15)
-    }
+fun MenuItem(
+    @DrawableRes id: Int,
+    @StringRes title: Int,
+    onClick: () -> Unit,
+) {
 
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.clickable { onClick() }
+    ) {
+        IconSmall(id, tint = Color.White) { }
+        TextMedium(text = stringResource(id = title), color = Color.White)
+    }
 }
