90f943ba2939907863e8ddd8fdfd17a49d8fc76a
byron <1byron1999@gmail.com>
2025-02-10 03:03:07 -0800

    Add More fields in Playlist Entity

--- app/src/main/java/com/byrondev/musicplayer/data/models/Playlist.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/models/Playlist.kt
@@ -1,25 +1,39 @@
 package com.byrondev.musicplayer.data.models
 
+import androidx.compose.ui.graphics.toArgb
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.byrondev.musicplayer.utils.randomColor
 
-@Entity(tableName =  "playlist")
+@Entity(tableName = "playlist")
 data class Playlist(
 
     @PrimaryKey(autoGenerate = true)
-    val id : Int = 0,
+    val id: Int = 0,
 
     @ColumnInfo(name = "name")
-    val name : String = ""
+    val name: String = "",
+
+    @ColumnInfo(name = "number_of_songs")
+    val numberOfSongs : Int = 0,
+
+    @ColumnInfo(name = "duration")
+    val duration : Long = 0,
+
+    @ColumnInfo(name = "color1")
+    val color1: Int = randomColor().toArgb(),
+
+    @ColumnInfo(name = "color2")
+    val color2: Int = randomColor().toArgb(),
 )
 
-//PlaylistSongCrossRef table intermedie
+
 @Entity(
-    primaryKeys = ["songId","playlistId"],
+    primaryKeys = ["songId", "playlistId"],
     tableName = "playlist_song_cross_ref"
 )
-data class PlaylistSongCrossRef (
+data class PlaylistSongCrossRef(
     val songId: Int,
     val playlistId: Int,
 )
