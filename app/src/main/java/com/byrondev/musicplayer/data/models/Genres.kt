446baa1179ce3803fe8bac0edead626bebb8a9c7
byron <1byron1999@gmail.com>
2025-02-10 03:07:45 -0800

    Add more fields in Genre Entity

--- app/src/main/java/com/byrondev/musicplayer/data/models/Genres.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/models/Genres.kt
@@ -1,8 +1,10 @@
 package com.byrondev.musicplayer.data.models
 
+import androidx.compose.ui.graphics.toArgb
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.byrondev.musicplayer.utils.randomColor
 
 @Entity(tableName = "genres")
 data class Genre(
@@ -11,5 +13,16 @@ data class Genre(
     val id : Int = 0,
 
     @ColumnInfo(name = "name")
-    val name : String = ""
-)
+    val name : String? = "",
+
+    @ColumnInfo(name = "num_of_songs")
+    val numOfSongs : Long = 0,
+
+    @ColumnInfo(name = "color1")
+    val color1: Int = randomColor().toArgb(),
+
+    @ColumnInfo(name = "color2")
+    val color2: Int = randomColor().toArgb(),
+
+
+    )
