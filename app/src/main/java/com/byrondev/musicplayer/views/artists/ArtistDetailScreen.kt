275ee152440fe024c205d7b2c621719e2b529779
byron <1byron1999@gmail.com>
2025-02-10 03:05:47 -0800

    Replace songCard to SongCardWithCover in ArtistDetailScreen

--- app/src/main/java/com/byrondev/musicplayer/views/artists/ArtistDetailScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/artists/ArtistDetailScreen.kt
@@ -2,23 +2,16 @@ package com.byrondev.musicplayer.views.artists
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -26,21 +19,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import com.byrondev.musicplayer.R
 import com.byrondev.musicplayer.components.albums.AlbumsCarousel
 import com.byrondev.musicplayer.components.albums.ButtonsPlayAlbum
-import com.byrondev.musicplayer.components.globals.LazyImageCover
-import com.byrondev.musicplayer.components.songs.SongCard
+import com.byrondev.musicplayer.components.globals.LargeImage
+import com.byrondev.musicplayer.components.songs.SongCardWithCover
 import com.byrondev.musicplayer.components.texts.TextRowSeparation
 import com.byrondev.musicplayer.components.topbar.CenterTopAppBar
 import com.byrondev.musicplayer.ui.theme.Slate80
 import com.byrondev.musicplayer.viewModels.MusicViewModels
 import com.byrondev.musicplayer.viewModels.PlayerViewModels
-import kotlinx.coroutines.launch
 
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
@@ -69,11 +60,15 @@ fun ArtistDetailScreen(
     LaunchedEffect(songs.size) {
         playerViewModels.updateCurrentListSongs(songs)
     }
-    Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
         Column {
             CenterTopAppBar(
-                title = artist?.name ?: "Arist unknown",
-                Icons.AutoMirrored.Default.ArrowBack
+                title = artist?.name ?: stringResource(R.string.unknown_artist),
+                iconColor = Color.White
             ) { navController.popBackStack() }
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -83,16 +78,17 @@ fun ArtistDetailScreen(
                     modifier = Modifier.background(Slate80, shape = CircleShape),
                     contentAlignment = Alignment.Center
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.artist_icon),
-                        modifier = Modifier.padding(10.dp).size(150.dp),
-                        contentDescription = "",
-                    )
+                    LargeImage(id = R.drawable.artist_200, modifier = Modifier.padding(10.dp))
                 }
                 TextRowSeparation(
-                    text1 = "Canciones",
-                    text2 = "Ver todo",
-                    modifier = Modifier.padding(top = 30.dp, start = 15.dp, end = 20.dp, bottom = 10.dp),
+                    text1 = stringResource(R.string.library_songs),
+                    text2 = stringResource(R.string.see_more),
+                    modifier = Modifier.padding(
+                        top = 30.dp,
+                        start = 15.dp,
+                        end = 20.dp,
+                        bottom = 10.dp
+                    ),
                     onClick = { navController.navigate("SongsByArtist/${id}") }
                 )
                 Column(
@@ -100,20 +96,12 @@ fun ArtistDetailScreen(
                 ) {
                     ButtonsPlayAlbum(playerViewModels, modifier = Modifier.padding(bottom = 15.dp))
                     limitedItems.forEachIndexed { index, song ->
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            LazyImageCover(
-                                uri = song.uri,
-                                painterResource(R.drawable.image_music_default),
-                                modifier = Modifier.width(50.dp).aspectRatio(1f)
-                            )
-                            SongCard(song, false, navController) {
-                                playerViewModels.viewModelScope.launch {
-                                    playerViewModels.playSeekTo(index)
-                                }
-                            }
-                        }
+                        SongCardWithCover(
+                            song, index,
+                            navController,
+                            playerViewModels,
+                            musicViewModels
+                        )
                         Spacer(modifier = Modifier.height(10.dp))
                     }
                 }
@@ -121,8 +109,8 @@ fun ArtistDetailScreen(
                     AlbumsCarousel(
                         albumsType,
                         navController,
-                        text1 = "Albumes",
-                        text2 = "Ver todo"
+                        text1 = stringResource(R.string.library_albums),
+                        text2 = stringResource(R.string.see_more)
                     ) { navController.navigate("AlbumsByArtist/${id}") }
                 }
                 if (singles.isNotEmpty()) {
