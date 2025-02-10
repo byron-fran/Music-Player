fedcde19eecb60ea6c9896d06a810169b08406a6
byron <1byron1999@gmail.com>
2025-02-10 02:36:41 -0800

    Change arrow icon in TextSeparation and change color

--- app/src/main/java/com/byrondev/musicplayer/components/texts/TextRowSeparation.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/texts/TextRowSeparation.kt
@@ -4,17 +4,15 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.byrondev.musicplayer.R
+import com.byrondev.musicplayer.components.globals.IconSmall
 import com.byrondev.musicplayer.components.globals.TextLarge
 import com.byrondev.musicplayer.components.globals.TextMedium
-import com.byrondev.musicplayer.ui.theme.Zinc40
 
 
 @Composable
@@ -29,19 +27,14 @@ fun TextRowSeparation(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ){
-        TextLarge(text1)
+        TextLarge(text1,color= Color.White )
         if(text2.trim().isNotEmpty()) {
             Row (
                 modifier = Modifier.clickable { onClick() },
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-                TextMedium(text2, color = Zinc40,)
-                Icon(
-                    imageVector = Icons.AutoMirrored.Default.ArrowForward,
-                    modifier = Modifier.size(30.dp),
-                    contentDescription =  "Icon To view more",
-                    tint = Zinc40
-                )
+                TextMedium(text2)
+                IconSmall(R.drawable.arrow_right_alt_30) { }
             }
         }
     }
