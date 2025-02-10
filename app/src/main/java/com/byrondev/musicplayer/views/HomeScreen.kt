afd2f7d74256f8ba1b35c44bc5dde86d50ed563e
byron <1byron1999@gmail.com>
2025-02-10 03:10:17 -0800

    Change items to item in LazyColumn in HomeScreen

--- app/src/main/java/com/byrondev/musicplayer/views/HomeScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/HomeScreen.kt
@@ -7,13 +7,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.byrondev.musicplayer.R
 import com.byrondev.musicplayer.components.albums.AlbumsCarousel
@@ -33,38 +32,64 @@ fun HomeScreen(navController: NavController, musicViewModels: MusicViewModels) {
     val albums = musicViewModels.albums.collectAsState().value.sortedByDescending { it.id }
     val albumsFavorites = musicViewModels.albumsFavorites.collectAsState()
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Black), ) {
+    Box(
+        modifier = Modifier.fillMaxSize().background(Color.Black),
+    ) {
         Column {
-            CenterTopAppBar ("Home", Icons.Default.Settings){ navController.navigate("SettingsScreen") }
-            LazyColumn(modifier = Modifier.fillMaxSize())
-            {
-                items(1) {
-                    if(songs.value.isEmpty()) {
+            CenterTopAppBar(
+                stringResource(id = R.string.home_name),
+                iconColor = Color.White,
+                id = R.drawable.settings_30,
+            ) { navController.navigate("SettingsScreen") }
+            LazyColumn {
+                item {
+                    if (songs.value.isEmpty()) {
                         SelectedLaunchedFiles(musicViewModels) {
-                            CardHome(onClick = { it.launch(null) }, "Agregar música", painterResource(id = R.drawable.baseline_add_30))
+                            CardHome(
+                                onClick = { it.launch(null) },
+                                stringResource(R.string.add_music),
+                                painterResource(id = R.drawable.baseline_add_30)
+                            )
                         }
                     }
-                    if(albums.isNotEmpty()) {
-                        AlbumsCarousel(albums.take(10), navController, text1 = "Agrego recientemente", text2 = "") { }
+                    if (albums.isNotEmpty()) {
+                        AlbumsCarousel(
+                            albums.take(10),
+                            navController,
+                            text1 = stringResource(R.string.recently_added),
+                            text2 = ""
+                        ) { }
                     }
-                    if(playlistItems.value.isNotEmpty()) {
+                    if (playlistItems.value.isNotEmpty()) {
                         PlaylistCarousel(playlistItems.value.take(5), navController)
 
-                    }
-                    else {
-                        CardHome(onClick = { musicViewModels.onChangeValueModal()}, "Agregar nueva lista de reproduccion", painterResource(id=R.drawable.baseline_add_30))
+                    } else {
+                        CardHome(
+                            onClick = { musicViewModels.onChangeValueModal() },
+                            stringResource(R.string.add_new_playlist),
+                            painterResource(id = R.drawable.baseline_add_30)
+                        )
                         PlaylistModal(musicViewModels)
                     }
-                    if(albumsFavorites.value.isNotEmpty()) {
-                        AlbumsCarousel(albumsFavorites.value.take(10), navController, text1 = "Albumes favoritos", text2 = "ver más") {navController.navigate("FavoritesScreen") }
+                    if (albumsFavorites.value.isNotEmpty()) {
+                        AlbumsCarousel(
+                            albumsFavorites.value.take(10),
+                            navController,
+                            text1 = stringResource(R.string.favorites_albums),
+                            text2 = stringResource(R.string.see_more)
+                        ) { navController.navigate("FavoritesScreen") }
+                    } else {
+                        CardHome(
+                            onClick = { navController.navigate("FavoritesScreen") },
+                            stringResource(R.string.favorites),
+                            painterResource(id = R.drawable.favorite_filled)
+                        )
                     }
-                    else {
-                        CardHome(onClick = { navController.navigate("FavoritesScreen") }, "Favoritos", painterResource(id=R.drawable.favorite_filled))
-                    }
-
-                    CardHome(onClick = { /* Todo add event*/ }, "Historial de reproducciones", painterResource(id=R.drawable.baseline_cd_30))
+                    CardHome(
+                        onClick = { /* Todo add event*/ },
+                        "Historial de reproducciones",
+                        painterResource(id = R.drawable.baseline_cd_30)
+                    )
                 }
             }
         }
