ca4da46e877f76ead624fb8545ed0c7b21b08945
byron <1byron1999@gmail.com>
2025-02-15 06:51:17 -0800

    Remove exoplayer initialization in MainActivity

--- app/src/main/java/com/byrondev/musicplayer/MainActivity.kt
+++ app/src/main/java/com/byrondev/musicplayer/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
-import androidx.media3.common.AudioAttributes
-import androidx.media3.common.C
-import androidx.media3.exoplayer.ExoPlayer
-import com.byrondev.musicplayer.navigation.NavManager
+import com.byrondev.musicplayer.layout.LayoutScreen
 import com.byrondev.musicplayer.ui.theme.MusicPlayerTheme
 import com.byrondev.musicplayer.viewModels.MusicViewModels
 import com.byrondev.musicplayer.viewModels.PlayerViewModels
@@ -19,41 +16,19 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private lateinit var player: ExoPlayer
 
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        val viewModels: MusicViewModels by viewModels()
-        val playerViewModels : PlayerViewModels by viewModels()
-        // Create the ExoPlayer instance
-        player = ExoPlayer.Builder(this)
-            .setAudioAttributes(
-                AudioAttributes.Builder()
-                    .setUsage(C.USAGE_MEDIA)
-                    .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
-                    .build(),
-                true
-            )
-            .setHandleAudioBecomingNoisy(true)
-            .build()
-        playerViewModels.setPlayer(player)
+        val musicViewModels: MusicViewModels by viewModels()
+        val playerViewModels: PlayerViewModels by viewModels()
         enableEdgeToEdge()
 
         setContent {
             MusicPlayerTheme {
-                NavManager(viewModels, player, playerViewModels)
+                LayoutScreen(playerViewModels, musicViewModels)
             }
         }
-
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        player.release()
     }
 }
-
-
-
-
-
