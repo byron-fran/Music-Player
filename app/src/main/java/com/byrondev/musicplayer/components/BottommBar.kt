41b0e95e33f6fe2a86807d7650f54d9f40e1d5fb
byron <1byron1999@gmail.com>
2025-02-15 05:58:25 -0800

    Change color icon to gray and change title

--- app/src/main/java/com/byrondev/musicplayer/components/BottommBar.kt
+++ app/src/main/java/com/byrondev/musicplayer/components/BottommBar.kt
@@ -3,7 +3,6 @@ package com.byrondev.musicplayer.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -17,34 +16,52 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.byrondev.musicplayer.R
+import com.byrondev.musicplayer.ui.theme.Gray
 import com.byrondev.musicplayer.ui.theme.Rose60
 import com.byrondev.musicplayer.ui.theme.Slate70
-import com.byrondev.musicplayer.ui.theme.Zinc40
 import com.byrondev.musicplayer.ui.theme.textSmall
 
 data class MenuItemBottom(
     val title: String,
-    val icon : Painter,
-    val size : Dp = 30.dp,
-    val path : String = ""
+    val icon: Painter,
+    val size: Dp = 30.dp,
+    val path: String = "",
 
-)
+    )
+
 @Composable
 fun BottomBar(navController: NavController, modifier: Modifier = Modifier) {
 
     val listMenuItemBottom = listOf(
-        MenuItemBottom(title = "Home", icon = painterResource(id=R.drawable.home), path = "HomeScreen" ),
-        MenuItemBottom(title = "Library", icon = painterResource(id=R.drawable.music), path = "LibraryScreen"),
-        MenuItemBottom(title = "Favorites", icon = painterResource(id=R.drawable.favorite_filled), path="FavoritesScreen"),
-        MenuItemBottom(title = "Search", icon = painterResource(id=R.drawable.search), path = "SearchScreen"),
+        MenuItemBottom(
+            title = stringResource(id = R.string.tab_home),
+            icon = painterResource(id = R.drawable.home),
+            path = "HomeScreen"
+        ),
+        MenuItemBottom(
+            title = stringResource(id = R.string.tab_library),
+            icon = painterResource(id = R.drawable.music),
+            path = "LibraryScreen"
+        ),
+        MenuItemBottom(
+            title = stringResource(id = R.string.tab_favorite),
+            icon = painterResource(id = R.drawable.favorite_filled),
+            path = "FavoritesScreen"
+        ),
+        MenuItemBottom(
+            title = stringResource(id = R.string.tab_search),
+            icon = painterResource(id = R.drawable.search),
+            path = "SearchScreen"
+        ),
     )
     val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
-    NavigationBar (
+    NavigationBar(
         modifier = modifier
             .background(color = Color.DarkGray)
             .drawBehind {
@@ -60,24 +77,36 @@ fun BottomBar(navController: NavController, modifier: Modifier = Modifier) {
             .height(50.dp),
         containerColor = Color.Black,
 
-    ) {
+        ) {
         listMenuItemBottom.forEach { item ->
             NavigationBarItem(
                 selected = item.path == currentRoute,
                 onClick = { navController.navigate(item.path) },
-                icon = { Icon(item.icon, contentDescription = "Icon ${item.title}", modifier = Modifier.offset(x= 0.dp, y=17.dp)) },
-                label = { Text(item.title, style = textSmall,modifier = Modifier.offset(x= 0.dp, y = 8.dp)) },
-                modifier = Modifier
-                    .size(item.size)
-                    .align(Alignment.Top),
-                colors =  NavigationBarItemColors(
+                icon = {
+                    Icon(
+                        item.icon,
+                        contentDescription = "Icon ${item.title}",
+                        modifier = Modifier.offset(x = 0.dp, y = 17.dp)
+                    )
+                },
+                label = {
+                    Text(
+                        item.title,
+                        color = Color.Unspecified,
+                        style = textSmall,
+                        modifier = Modifier
+                            .offset(x = 0.dp, y = 8.dp),
+                    )
+                },
+                modifier = Modifier.align(Alignment.Top),
+                colors = NavigationBarItemColors(
                     selectedIconColor = Rose60,
                     selectedTextColor = Rose60,
-                    disabledIconColor = Zinc40,
-                    disabledTextColor = Zinc40,
+                    disabledIconColor = Gray,
+                    disabledTextColor = Gray,
                     selectedIndicatorColor = Color.Transparent,
-                    unselectedIconColor = Zinc40,
-                    unselectedTextColor = Zinc40
+                    unselectedIconColor = Gray,
+                    unselectedTextColor = Gray
                 )
             )
         }
