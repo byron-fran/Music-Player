630d7a4231aad681af92890e28f08706bcf96b49
byron <1byron1999@gmail.com>
2025-02-15 05:50:25 -0800

    Change layout scaffold to Box container in PlaylistScreen and AlbumsByArtistScreen.kt

--- app/src/main/java/com/byrondev/musicplayer/views/artists/AlbumsByArtistScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/artists/AlbumsByArtistScreen.kt
@@ -5,14 +5,14 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.byrondev.musicplayer.R
 import com.byrondev.musicplayer.components.albums.AlbumsList
 import com.byrondev.musicplayer.components.topbar.CenterTopAppBar
 import com.byrondev.musicplayer.viewModels.MusicViewModels
@@ -20,21 +20,28 @@ import com.byrondev.musicplayer.viewModels.MusicViewModels
 
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
-fun AlbumsByArtist(navController: NavController, musicViewModels: MusicViewModels, id : Int) {
+fun AlbumsByArtist(
+    navController: NavController,
+    musicViewModels: MusicViewModels,
+    id: Int,
+) {
 
-    val artistWithAlbums  by musicViewModels.artistWithAlbums.collectAsState()
-    val albums = artistWithAlbums.sortedByDescending {  it.year.substringBefore("-")?.toInt()  } ?: emptyList()
+    val artistWithAlbums by musicViewModels.artistWithAlbums.collectAsState()
+    val albums = artistWithAlbums.sortedByDescending { it.year.substringBefore("-").toInt() }
 
     LaunchedEffect(id) {
         musicViewModels.getArtistWithAlbums(id)
     }
 
-    Box (
+    Box(
         modifier = Modifier.fillMaxSize()
-    ){
+    ) {
         Column {
-            CenterTopAppBar("Albums",  Icons.AutoMirrored.Default.ArrowBack, onNavigate = {navController.popBackStack()})
-            AlbumsList(albums , navController )
+            CenterTopAppBar(
+                stringResource(id = R.string.library_albums),
+                onNavigate = { navController.popBackStack() }
+            )
+            AlbumsList(albums, navController)
         }
     }
 }
--- app/src/main/java/com/byrondev/musicplayer/views/playlist/PlaylistScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/playlist/PlaylistScreen.kt
@@ -2,10 +2,15 @@ package com.byrondev.musicplayer.views.playlist
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.byrondev.musicplayer.R
 import com.byrondev.musicplayer.components.playlist.PlaylistItems
 import com.byrondev.musicplayer.components.topbar.CenterTopAppBar
 import com.byrondev.musicplayer.viewModels.MusicViewModels
@@ -13,12 +18,26 @@ import com.byrondev.musicplayer.viewModels.MusicViewModels
 
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
-fun PlaylistScreen(musicViewModels: MusicViewModels, navController: NavController, songId : Int = 0) {
+fun PlaylistScreen(
+    musicViewModels: MusicViewModels,
+    navController: NavController,
+    songId: Int = 0,
+) {
 
-    Scaffold (
-        topBar = { CenterTopAppBar("Playlists"){navController.popBackStack()} },
-        containerColor = Color.Black
-    ) { paddingValues ->
-        PlaylistItems(musicViewModels, navController, paddingValues, songId)
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column {
+            CenterTopAppBar(
+                title = stringResource(R.string.library_playlists),
+                iconColor = Color.White
+            ) {
+                navController.popBackStack()
+            }
+            PlaylistItems(
+                musicViewModels,
+                navController,
+                songId = songId
+            )
+
+        }
     }
 }
