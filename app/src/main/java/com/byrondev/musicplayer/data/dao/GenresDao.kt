0a955a5e31a37dda731ad0e59f26a914412f0a2a
byron <1byron1999@gmail.com>
2025-02-15 06:48:44 -0800

    Update query to get genres and add Join table

--- app/src/main/java/com/byrondev/musicplayer/data/dao/GenresDao.kt
+++ app/src/main/java/com/byrondev/musicplayer/data/dao/GenresDao.kt
@@ -10,7 +10,13 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface GenresDao {
 
-    @Query("SELECT * FROM genres")
+    @Query("""
+        SELECT g.id, g.name, g.color1, g.color2,
+        COUNT(s.id) as num_of_songs
+        FROM genres g
+        LEFT JOIN songs s ON s.genre = g.name
+        GROUP BY g.name ORDER BY g.name DESC
+    """)
     fun getAllGenres () : Flow<List<Genre>>
 
     @Query("SELECT * FROM genres WHERE name = :name COLLATE NOCASE LIMIT 1")
